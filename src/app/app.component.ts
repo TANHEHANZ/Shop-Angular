9538ab0351609ac6b13810187366b2498e99ab49
TANHEHANZ <hantach10@gmail.com>
2025-01-25 08:34:19 -0400

    feat: create proyect add feat signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  computed,
+  Signal,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
+  standalone: true,
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'PGproyect';
+  title = 'Proyecto Angular';
+  count: WritableSignal<number> = signal(0);
+
+  increment() {
+    this.count.update((value) => value + 1);
+  }
+  dobleCount: Signal<number> = computed(() => this.count() * 2);
 }
---
+++ src/app/precentation/layouts/layout-private/layout-private.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-private',
+  imports: [],
+  templateUrl: './layout-private.component.html',
+  styleUrl: './layout-private.component.css'
+})
+export class LayoutPrivateComponent {
+
+}
