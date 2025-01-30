3daed3182246a45e568acedc3465c3ef0ee04b43
TANHEHANZ <hantach10@gmail.com>
2025-01-29 23:22:47 -0400

    faet: create directive atribute

---
+++ src/app/components/directive-atributo/directive-atributo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directive-atributo',
+  imports: [],
+  templateUrl: './directive-atributo.component.html',
+  styleUrl: './directive-atributo.component.css'
+})
+export class DirectiveAtributoComponent {
+
+}
