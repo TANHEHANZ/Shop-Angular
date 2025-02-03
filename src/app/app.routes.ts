001c90d5a282d44ab1e50d7147c8471cb2183692
TANHEHANZ <hantach10@gmail.com>
2025-02-02 22:23:27 -0400

    routing con guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { SenalComponent } from './components/senal/senal.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'signal ', component: SenalComponent }];
